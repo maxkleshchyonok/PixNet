ad617629f34d46722232b60e1d607dd09c190d2b
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2022-03-29 12:21:13 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
+import { SortComponent } from './sort/sort.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileInfoComponent,
+    SortComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
