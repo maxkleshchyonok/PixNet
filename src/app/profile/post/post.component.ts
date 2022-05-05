d39f5909ddf9985adc0ea6fc9bffa12412e315a1
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2022-05-05 22:18:41 +0300

    like full functional

--- src/app/profile/post/post.component.ts
+++ src/app/profile/post/post.component.ts
@@ -7,7 +7,7 @@ import {Collections} from "../../../services/crud/collections";
 import {Observable} from "rxjs";
 import {AuthService} from "../../../services/auth/auth.service";
 import {Post, PostStore, User, UserStore} from "../../../services/types";
-import {filter, map, switchMap} from "rxjs/operators";
+import {filter, map, switchMap, tap} from "rxjs/operators";
 import Firestore = firebase.firestore.Firestore;
 import {PopUpComponent} from "../pop-up/pop-up.component";
 import {MatDialog} from "@angular/material/dialog";
@@ -18,15 +18,21 @@ import {PostModalComponent} from "../post-modal/post-modal.component";
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
 
   public posts: Observable<PostStore[]> = this.crudService.handleData<PostStore>(Collections.POSTS);
 
   public isLike: boolean = false;
 
+  public userId: string | undefined = '';
+
   constructor(private authService: AuthService,
               private crudService: CrudService,
               private dialogRef: MatDialog) {
+  }
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((value: firebase.User | null) => this.userId = value?.uid!)
   }
 
   public delete(id: string): void {
@@ -34,21 +40,18 @@ export class PostComponent {
     this.crudService.deleteObject(Collections.POSTS, id).subscribe();
   }
 
-  // onClick() {
-  //   this.isLike = !this.isLike
-  // }
+  public change() {
+    this.isLike = !this.isLike
+  }
 
   public update(id: string): void {
     const post: Post = {
       image: "https://media.istockphoto.com/photos/melbourne-central-business-district-picture-id600688368?k=20&m=600688368&s=612x612&w=0&h=hbN7pEOSGuyzbygdh-vgj5mmBeGne2NHDYlojpfmoTw=",
       postDescr: "Visited Melbourne",
       likes: [],
-      // isLiked: false
     }
     this.crudService.updateObject(Collections.POSTS, id, post).subscribe();
   }
-
-  // public posts: Post[] = Posts;
 
   public updateLikes(id: string): any {
     console.log(id)
@@ -61,6 +64,7 @@ export class PostComponent {
             console.log(userIndex);
             if (userIndex === -1) {
               this.isLike = true;
+              console.log(this.isLike)
               return post?.likes.concat(value?.uid!)
             } else {
               const newArr: any = post?.likes.splice(userIndex! ,1);
@@ -70,7 +74,7 @@ export class PostComponent {
             }
           }),
           switchMap(likes => this.crudService.updateObject(Collections.POSTS, id, {likes})
-          )
+      )
         )
       })
     ).subscribe();
--- src/services/types.ts
+++ src/services/types.ts
@@ -3,12 +3,6 @@ import firebase from "firebase/compat/app";
 export type FireBaseUser = firebase.User | null;
 export type UserCredential = firebase.auth.UserCredential;
 
-// export type User = {
-//   name: string;
-//   surname: string;
-//   email: string
-// };
-
 export type User = {
   id: string;
   img: string;
@@ -24,7 +18,6 @@ export type Post = {
   image: string | null;
   postDescr: string | null;
   likes: string[];
-  // isLiked: boolean
 }
 
 export type UserStore = User & ID | null;
